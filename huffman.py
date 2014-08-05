743f758c09cdebfafa38f7db96de84e2caa0114f
Michael Krisper <michael.krisper@gmail.com>
2014-08-05 20:32:28 +0200

    Optimizations (Lists, heapq, TreeNode)
    Several optimisations:
    * Using Lists of Integers instead of Strings for Binary Representation
    * heapq instead of PriorityQueue
    * TreeNode Object instead of Inline Tuple

--- huffman.py
+++ huffman.py
@@ -1,115 +1,114 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Module for encoding/decoding Data with the Huffman-Coding"""
+"""Example script for encoding/decoding data with Huffman-coding
+Usage:
+1) with default lorem-ipsum text: python huffman.py
+2) with an input file to encode:  python huffman.py <inputfile>
+"""
 
 import Queue
+import collections
+import heapq
+import sys
 
 __author__ = "Michael Krisper"
-__email__ = "michael.krisper@htu.tugraz.at"
-__date__ = "2012-05-20"
+__email__ = "michael.krisper@gmail.com"
+__date__ = "2014-08-05"
 
-def convert_huffman_tree_to_code(node, prefix = 1):
-    """Traverses all nodes in a tree and converts it to a code. left=1, right=0"""
-    code = {}    
+class TreeNode(object):
+    def __init__(self, value=None, count=1, left=None, right=None, code = ""):
+        self.count = count
+        self.value = value
+        self.left = left
+        self.right = right
+        self.code = code
+
+    def isLeaf(self):
+        return not (self.left or self.right)
+
+    def __lt__(self, other):
+        return self.count < other.count
+
+    def __add__(self, other):
+        self.code = 0
+        other.code = 1
+        return TreeNode(None, self.count + other.count, self, other)
+
+    def __repr__(self):
+        return "TreeNode(%r, %r)" % (self.value, self.count)
+
+def convert_huffman_tree_to_code(node, prefix=[]):
+    """Traverses all nodes in a tree and converts it to a code."""
+    code = {}
     if node:
-        if len(node[1]) > 0:
-            code[node[1]] = prefix
-        if node[2]:
-            code.update(convert_huffman_tree_to_code(node[2], (prefix << 1)))
-        if node[3]:
-            code.update(convert_huffman_tree_to_code(node[3], (prefix << 1) | 1))
+        prefix = prefix + [node.code]
+        if node.isLeaf():
+            code[node.value] = prefix
+        else:
+            code.update(convert_huffman_tree_to_code(node.left, prefix))
+            code.update(convert_huffman_tree_to_code(node.right, prefix))
     return code
-
-def build_huffman_tree(text, keylength=1):
-    """
-    Builds a binary tree representing the letter frequency of the text.
-    Tree consists of tuples: (sortindex, character, left child, right child)
-    Internal nodes have character "", leaves have the respective encoded character.
-    """
-    charcount = {}
-    
-    pos = 0    
-    while pos < len(text):
-        key = text[pos:pos + keylength]
-        charcount[key] = charcount.setdefault(key, 0) + 1
-        pos += keylength
-    
-    prioq = Queue.PriorityQueue()
-    for item in charcount.iteritems():
-        prioq.put_nowait((item[1], item[0], None, None)) 
-    
-    while prioq.qsize() > 1:
-        left = prioq.get_nowait()
-        right = prioq.get_nowait()
-        internal_node = (left[0] + right[0], "", left, right)
-        prioq.put_nowait(internal_node)
-        
-    return prioq.get_nowait()
 
 def get_huffman_code(text):
     """Returns a Code-Dictionary { key=Character, value=Code }"""
-    tree = build_huffman_tree(text)
+
+    counter = collections.Counter(text)
+    heap = [TreeNode(item, count) for item,count in counter.iteritems()]
+
+    heapq.heapify(heap)
+    
+    while len(heap) >= 2:
+        heapq.heappush(heap, heapq.heappop(heap)+heapq.heappop(heap))
+
+    tree = heap[0]    
     return convert_huffman_tree_to_code(tree)
 
 def encrypt_text(text, code):
     """Encrypts a text with huffman-code"""
-    crypted = 1
-    pos = 0
-    keylength = len(code.keys()[0])
-    while pos < len(text):
-        key = text[pos:pos + keylength]
-        crypted = crypted << len(bin(code[key])[3:]) + int(bin(code[key])[3:],2)
-        #crypted.append(bin(code[key])[3:])
-        pos += keylength
-        
-    return crypted
+    crypted = []
+    for char in text:
+        crypted.extend(code[char])
+
+    return "".join(str(v) for v in crypted)
 
 def decrypt_text(text, code):
     """Decrypts a text with huffman-code"""
     decrypted = []
-    pos = 0
-    text = bin(text)[2:]
-    print text
-    while pos < len(text):
-        for key, value in code.iteritems():
-            if text[pos:].startswith(bin(value)[3:]):
-                decrypted.append(key)
-                pos += len(bin(value)[3:])
-                break
-        else:
-            raise KeyError
+    reverse_code = dict(zip(["".join([str(y) for y in v]) for v in code.values()], code.keys()))
+
+    current = ""
+    for c in text:
+        current += c
+        if current in reverse_code:
+            decrypted.append(reverse_code[current])
+            current = ""
     return "".join(decrypted)
 
 def main(text):
     """Shows sample usage of the functions for the Huffman-code."""
-    print text
+
     code = get_huffman_code(text)
     
-    # print code
-    for key, value in sorted(code.iteritems(), key=lambda (k, v): (len(k), k)):
-        print "%s: %s" % (key, bin(value)[3:])
+    # print the code
+    for key, value in sorted(code.iteritems(), key=lambda (k, v): (len(v), k)):
+        print "%s: %s" % (key, "".join(str(v) for v in value))
 
     crypted_text = encrypt_text(text, code)
-    print crypted_text
-    
     decrypted_text = decrypt_text(crypted_text, code)
-    
     assert decrypted_text == text
     
-    
-    
-    code_text = ""
-    for key, value in code.iteritems():
-        code_text += (key * 8) + ":" + bin(value)[2:] + "\n"
-        
+    code_text = str(code)
     result = (len(code_text),
-              len(text) * 8, len(bin(crypted_text)[2:]),
-              len(bin(crypted_text)[2:]) / (len(text) * 8.0) * 100,
-              len(bin(crypted_text)[2:] + code_text) / (len(text) * 8.0) * 100)
-    print "Code Length: %d, Text Length: %d, Crypted Text Length: %d - Percentage: %.1f %% / %.1f %% with Code" % result
+              len(text) * 8, len(crypted_text),
+              len(crypted_text) / (len(text) * 8.0) * 100,
+              len(crypted_text + code_text) / (len(text) * 8.0) * 100)
+    print "Code Length: %d, Text Length: %d, Encrypted Text Length: %d - Percentage: %.1f %% / (%.1f %% with Code)" % result
 
 if __name__ == "__main__":
-    TEXT = """Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et 
+    if len(sys.argv) > 1:
+        TEXT = open(sys.argv[1], "rb").read()
+    else:
+        TEXT = """Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et 
 dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 
 gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
 elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos 
